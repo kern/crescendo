b56a88932e26967f048cdff433a91103e5fa14a0
Alex Kern <alex@kernul.com>
2013-06-26 20:28:16 -0700

    Add the HTTPS protocol.

--- src/piano/protocol.clj
+++ src/piano/protocol.clj
@@ -3,9 +3,17 @@
 (defprotocol Protocol
   (major [protocol])
   (minor [protocol])
+  (scheme [protocol])
   (secure? [protocol]))
 
 (def http-1-1 (reify Protocol
                 (major [protocol] 1)
                 (minor [protocol] 1)
+                (scheme [protocol] "http")
                 (secure? [protocol] false)))
+
+(def https-1-1 (reify Protocol
+                 (major [protocol] 1)
+                 (minor [protocol] 1)
+                 (scheme [protocol] "https")
+                 (secure? [protocol] true)))
