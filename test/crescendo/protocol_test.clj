dc3ef99eb0ad31b1d9d18bfb7a9a4f84f307d88a
Alex Kern <alex@kernul.com>
2013-07-03 00:04:25 -0700

    Use a simpler form of expect when it fits.

--- test/crescendo/protocol_test.clj
+++ test/crescendo/protocol_test.clj
@@ -7,14 +7,14 @@
 (expect -1 (:major (protocol {:major -1})))
 (expect -2 (:minor (protocol {:minor -2})))
 (expect "foo" (:scheme (protocol {:scheme "foo"})))
-(expect true (:secure? (protocol {:secure? true})))
+(expect (:secure? (protocol {:secure? true})))
 
 (expect 1 (:major http-1-1))
 (expect 1 (:minor http-1-1))
 (expect "http" (:scheme http-1-1))
-(expect false (:secure? http-1-1))
+(expect (not (:secure? http-1-1)))
 
 (expect 1 (:major https-1-1))
 (expect 1 (:minor https-1-1))
 (expect "https" (:scheme https-1-1))
-(expect true (:secure? https-1-1))
+(expect (:secure? https-1-1))
--- test/crescendo/status_code_test.clj
+++ test/crescendo/status_code_test.clj
@@ -4,29 +4,29 @@
 
 (expect informational? 100)
 (expect informational? 199)
-(expect (complement informational?) 200)
+(expect (not (informational? 200)))
 
 (expect successful? 200)
 (expect successful? 299)
-(expect (complement successful?) 199)
-(expect (complement successful?) 300)
+(expect (not (successful? 199)))
+(expect (not (successful? 300)))
 
 (expect redirection? 300)
 (expect redirection? 399)
-(expect (complement redirection?) 299)
-(expect (complement redirection?) 400)
+(expect (not (redirection? 299)))
+(expect (not (redirection? 400)))
 
 (expect client-error? 400)
 (expect client-error? 499)
-(expect (complement client-error?) 399)
-(expect (complement client-error?) 500)
+(expect (not (client-error? 399)))
+(expect (not (client-error? 500)))
 
 (expect server-error? 500)
 (expect server-error? 599)
-(expect (complement server-error?) 499)
+(expect (not (server-error? 499)))
 
 (expect error? 400)
 (expect error? 499)
 (expect error? 500)
 (expect error? 599)
-(expect (complement error?) 399)
+(expect (not (error? 399)))
